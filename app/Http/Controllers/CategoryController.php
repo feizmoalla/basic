42afed92e02097e1499f3ae4f7ff87c17dcf4565
feizmoalla <fmoalla12@gmail.com>
2021-10-24 10:53:59 +0100

    middleware auth users access control

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     public function AllCat(){
 
         // $categories = DB::table('categories')
